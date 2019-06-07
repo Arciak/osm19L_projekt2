fe16527ea3eaf9f1a04828815688115b7a3158dc
Arciak <a.zarzecki@stud.elka.pw.edu.pl>
2019-06-07 23:12:44 +0200

    Final Version

--- src/osm19L_projekt2/main.java
+++ src/osm19L_projekt2/main.java
@@ -8,11 +8,6 @@ import gui_package.MainWindowGUI;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		System.out.println("Program");
-//		DicomFile dicomFile = new DicomFile("/Volumes/Transcend/eclipse/IM-0001-0006.dcm");
-//		System.out.println("Koniec programu");
-//		dicomFile.printDataPatient();	
 		new MainWindowGUI();
 	}
 	
